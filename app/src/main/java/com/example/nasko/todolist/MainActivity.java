dbb7d5e7df554ad1f666c4e826ea2bc5fed175c5
Atanas <naskogizdov@yahoo.com>
2017-03-07 11:39:38 -0500

    Initial view and data structure

--- app/src/main/java/com/example/nasko/todolist/MainActivity.java
+++ app/src/main/java/com/example/nasko/todolist/MainActivity.java
@@ -8,8 +8,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // initialize variables for use cross-activity
+
+    private EditText editText;
+    private Button btn;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +33,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    // initialize UI elements and data structure
+        editText = (EditText) findViewById(R.id.userEntry);
+        btn = (Button) findViewById(R.id.addBtn);
+        list = (ListView) findViewById(R.id.dataList);
+        arrayList = new ArrayList<String>();
+
+        // create adapter to translate data to UI
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        // Set data
+        list.setAdapter(adapter);
+
+        arrayList.add("Did this work?");
+        arrayList.add("Did this work?");
+        arrayList.add("Did this work?");
+        adapter.notifyDataSetChanged();
+
+        //handle insertion to list
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // grab entry and add to list
+                arrayList.add(editText.getText().toString());
+                // notify UI of change
+                adapter.notifyDataSetChanged();
             }
         });
+
+        //test data
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
